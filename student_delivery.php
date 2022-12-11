75473a8ff81deab25eaa29a841aa8da399727fde
Andrei Dumitras <andreidumitras>
2022-12-11 23:58:50 +0200

    de pe laptop, test

--- student_delivery.php
+++ student_delivery.php
@@ -195,7 +195,7 @@
 		$stmt->execute();
 		$result = $stmt->get_result()->fetch_assoc();
 		echo "helloooooo" . $result;
-
+// aiCI AM RAMAS CU EDITATUL... HAI CA SE POATE.
 		$conn->close();
 		return $path;
 	}
